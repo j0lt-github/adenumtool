1d0c737058039f5cb4707c17f68689a9eedf1cdf
manmeet-j0lt <j0lt@thej0lt.com>
2023-08-29 06:59:50 +0530

    Add project files.

---
+++ adenumtool/EnumerationClass.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace adenumtool
+{
+    class EnumerationClass
+    {
+        public string output { get; set; }
+
+        public void GetDomain()
+        {
+            try
+            {
+                Domain domain = Domain.GetCurrentDomain();
+
+                var dcNames = from DomainController dc in domain.DomainControllers
+                              select dc.Name;
+
+                var childNames = from Domain child in domain.Children
+                                 select child.Name;
+
+                string result = $"###################### Current Domain Information ######################\n" +
+                                    $"Forest                  : {domain.Forest.Name}\n" +
+                                    $"DomainControllers       : {{ {string.Join(", ", dcNames)} }}\n" +
+                                    $"Children                : {{ {string.Join(", ", childNames)} }}\n" +
+                                    $"DomainMode              : {domain.DomainMode}\n" +
+                                    $"DomainModeLevel         : {(int)domain.DomainMode}\n" +
+                                    $"Parent                  : {domain.Parent}\n" +
+                                    $"PdcRoleOwner            : {domain.PdcRoleOwner}\n" +
+                                    $"RidRoleOwner            : {domain.RidRoleOwner}\n" +
+                                    $"InfrastructureRoleOwner : {domain.InfrastructureRoleOwner}\n" +
+                                    $"Name                    : {domain.Name}\n" +
+                                    "--------------------------------------------------";
+
+                output += result + "\n";
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: Fetching Current Domain Information");
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        public void GetForestDomain()
+        {
+            try
+            {
+                Forest currentForest = Forest.GetCurrentForest();
+                
+                foreach (Domain domain in currentForest.Domains)
+                {
+                    var dcNames = from DomainController dc in domain.DomainControllers
+                                  select dc.Name;
+
+                    var childNames = from Domain child in domain.Children
+                                     select child.Name;
+                    string result = $"###################### Forest Domain Information For Domain -  {domain.Name} ######################\n" +
+                                    $"Forest                  : {domain.Forest.Name}\n" +
+                                    $"DomainControllers       : {{ {string.Join(", ", dcNames)} }}\n" +
+                                    $"Children                : {{ {string.Join(", ", childNames)} }}\n" +
+                                    $"DomainMode              : {domain.DomainMode}\n" +
+                                    $"DomainModeLevel         : {(int)domain.DomainMode}\n" +
+                                    $"Parent                  : {domain.Parent}\n" +
+                                    $"PdcRoleOwner            : {domain.PdcRoleOwner}\n" +
+                                    $"RidRoleOwner            : {domain.RidRoleOwner}\n" +
+                                    $"InfrastructureRoleOwner : {domain.InfrastructureRoleOwner}\n" +
+                                    $"Name                    : {domain.Name}\n" +
+                                    "--------------------------------------------------";
+
+                    output += result + "\n";
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: Fetching Forest Domain Information");
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        public void GetDomainControllerAll()
+        {
+            try
+            {
+                Forest currentForest = Forest.GetCurrentForest();
+
+                foreach (Domain domain in currentForest.Domains)
+                {
+                    foreach (DomainController dc in domain.DomainControllers)
+                    {
+                        string result = $"########## Domain Controller Information For DC -  {dc.Name} for Domain - {domain.Name} ##########\n" +
+                            $"Forest    : {dc.Forest}\n" +
+                                        $"Domain    : {dc.Domain}\n" +
+                                        $"Name      : {dc.Name}\n" +
+                                        $"OSVersion : {dc.OSVersion}\n" +
+                                        "--------------------------------------------------";
+                        output += result + "\n";
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: Fetching All Domain Controller Information");
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        public void GetDomainController()
+        {
+            try
+            {
+                Domain domain = Domain.GetCurrentDomain();
+
+                foreach (DomainController dc in domain.DomainControllers)
+                {
+                    string result = $"###################### Domain Controller Information For DC -  {dc.Name} for Domain - {domain.Name} ######################" +
+                        $"Forest    : {dc.Forest}\n" +
+                                    $"Domain    : {dc.Domain}\n" +
+                                    $"Name      : {dc.Name}\n" +
+                                    $"OSVersion : {dc.OSVersion}\n" +
+                                    "--------------------------------------------------";
+                    output += result + "\n";
+                    Console.WriteLine(result);
+                }
+            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: Fetching Domain Controller Information of Current Domain");
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+
---
+++ adenumtool/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.Net;
+using System.Security.Cryptography;
+using System.Dynamic;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+
+namespace adenumtool
+{
+    internal class Program
+    {
+        private static HashSet<string> ValidInputs = new HashSet<string>() {
+            "Get-Domain",
+            "Get-ForestDomain",
+            "Get-DomainController",
+            "Get-DomainControllerAll",
+            "EveryThing",
+            "NeedOutput"
+        };
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a valid command.");
+                return;
+            }
+            if (args.Any(arg => !ValidInputs.Contains(arg)))
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+            EnumerationClass enumerationClass = new EnumerationClass();
+            foreach (string input in args) {
+            switch (input)
+                {
+                    case "Get-Domain":
+                        enumerationClass.GetDomain();
+                        break;
+                    case "Get-ForestDomain":
+                        enumerationClass.GetForestDomain();
+                        break;
+                    case "Get-DomainController":
+                        enumerationClass.GetDomainController();
+                        break;
+                    case "Get-DomainControllerAll":
+                        enumerationClass.GetDomainControllerAll();
+                        break;
+                    case "EveryThing":
+                        enumerationClass.GetDomain();
+                        enumerationClass.GetForestDomain();
+                        enumerationClass.GetDomainControllerAll();
+                        break;
+                }
+            }
+            if (args.Contains("NeedOutput"))
+            {
+                File.WriteAllText("./output.txt", enumerationClass.output);
+            }
+        }
+    }
+}
